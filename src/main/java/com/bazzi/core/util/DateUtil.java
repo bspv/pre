94fbb3f4a29089f5f058fe185bc0032584397f40
bspv <a08104124>
2021-07-08 10:37:43 +0800

    DateUtil修改

--- src/main/java/com/bazzi/core/util/DateUtil.java
+++ src/main/java/com/bazzi/core/util/DateUtil.java
@@ -4,12 +4,18 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public final class DateUtil {
     public static final String YMD_FORMAT = "yyyy-MM-dd";
     public static final String FULL_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
     public static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    private static final String SPACE_JOINER = " ";
+    private static final String TIME_PATTERN = "[[HH][:mm][:ss][.SSS]]";
 
     /**
      * 将日期格式化成字符串形式
@@ -35,8 +41,7 @@ public final class DateUtil {
     public static LocalDateTime getDate(String strDate, String format) {
         if (strDate == null || "".equals(strDate) || format == null || "".equals(format))
             return null;
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(format);
-        return LocalDateTime.parse(strDate, dtf);
+        return LocalDateTime.parse(strDate, buildDateTimeFormatter(format));
     }
 
     /**
@@ -97,6 +102,55 @@ public final class DateUtil {
      */
     public static Date convertToDate(LocalDateTime ldt) {
         return Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    /**
+     * 两个日期之间的天数差，忽略时分秒
+     *
+     * @param d1 日期
+     * @param d2 日期
+     * @return 天数差
+     */
+    public static int dayDiff(Date d1, Date d2) {
+        return dayDiff(convertToLocalDateTime(d1), convertToLocalDateTime(d2));
+    }
+
+    /**
+     * 两个日期之间的天数差，忽略时分秒
+     *
+     * @param ldt1 日期
+     * @param ldt2 日期
+     * @return 天数差
+     */
+    public static int dayDiff(LocalDateTime ldt1, LocalDateTime ldt2) {
+        return Math.abs((int) ChronoUnit.DAYS.between(ldt1.toLocalDate(), ldt2.toLocalDate()));
+    }
+
+    /**
+     * 构建用于打印和解析日期时间对象的格式化程序
+     *
+     * @param format 日期格式
+     * @return DateTimeFormatter
+     */
+    private static DateTimeFormatter buildDateTimeFormatter(String format) {
+        String curFormat;
+        if (format == null || "".equals(format))//缺省的日期格式
+            curFormat = YMD_FORMAT + SPACE_JOINER + TIME_PATTERN;
+        else if (!format.contains(SPACE_JOINER)) //不包含时分秒
+            curFormat = format + SPACE_JOINER + TIME_PATTERN;
+        else {
+            String[] arr = format.split(SPACE_JOINER);
+            if (arr[0].contains("HH")) {//时分秒在前
+                curFormat = TIME_PATTERN + SPACE_JOINER + arr[1];
+            } else {
+                curFormat = arr[0] + SPACE_JOINER + TIME_PATTERN;
+            }
+        }
+        return new DateTimeFormatterBuilder().appendPattern(curFormat)
+                .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)//缺省0，小时
+                .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)//缺省0，分钟
+                .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)//缺省0，秒
+                .toFormatter();
     }
 
 }
